35891f92a82494dbb58a9f973a0bff17947c41f0
Janyk Steenbeek <me@janyk.dev>
2023-05-26 10:04:21 +0200

    Add @socialgouv/matomo-next to dependencies and initialize in _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import type { AppProps } from 'next/app'
+import { init } from "@socialgouv/matomo-next";
+import React, { useEffect } from "react";
+
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    init({ url: 'https://a.webmethod.nl', siteId: '16' });
+  }, []);
+
   return <Component {...pageProps} />
 }
